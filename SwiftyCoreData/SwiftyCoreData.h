78011f28210f2cce3fed3f2144dd745c4423aa34
Michał Wójtowicz <michal.wojtowicz@kissdigital.com>
2018-12-20 09:46:24 +0100

    Initial Commit

---
+++ SwiftyCoreData/SwiftyCoreData.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyCoreData.h
+//  SwiftyCoreData
+//
+//  Created by Michał Wójtowicz on 20/12/2018.
+//  Copyright © 2018 Michał Wójtowicz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyCoreData.
+FOUNDATION_EXPORT double SwiftyCoreDataVersionNumber;
+
+//! Project version string for SwiftyCoreData.
+FOUNDATION_EXPORT const unsigned char SwiftyCoreDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyCoreData/PublicHeader.h>
+
+
